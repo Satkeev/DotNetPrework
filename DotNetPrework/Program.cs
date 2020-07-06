d8245be62c364d1e5ab64af0b5b869bb87a19e04
Kamit Satkeev <kamit.satkeev@gmail.com>
2020-07-05 19:56:16 -0700

    Add project files.

---
+++ DotNetPrework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNetPrework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
